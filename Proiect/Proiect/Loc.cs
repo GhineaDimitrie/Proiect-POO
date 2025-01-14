a87c8cd9c38aa0b2a712330e5455f5ef14060df5
Dumitrele Raul <raul.dumitrele@student.upt.ro>
2025-01-14 14:00:30 +0200

    Add Clasa Loc

---
+++ Proiect/Proiect/Loc.cs
@@ -0,0 +1,12 @@
+﻿namespace Proiect;
+
+public class Loc
+{
+    public int Numar { get; set; }
+    public bool EsteRezervat { get; set; } = false;
+
+    public Loc(int numar)
+    {
+        Numar = numar;
+    }
+}
