f87caee59acb815196cbe92273718e9de9b672e3
Dumitrele Raul <raul.dumitrele@student.upt.ro>
2025-01-14 15:29:30 +0200

    Adaugare clasa Rezervare si modificare Program.cs

--- Proiect/Proiect/Program.cs
+++ Proiect/Proiect/Program.cs
@@ -6,7 +6,10 @@ class Program
     {
         Loc loc1 = new Loc(1);
         Console.WriteLine($"Loc {loc1.Numar}: {(loc1.EsteRezervat ? "Rezervat" : "Liber")}");
-
+        
+        Rezervare rezervare1=new Rezervare("Ion Popescu", loc1.Numar,false);
+        Console.WriteLine($"Rezervare facuta de {rezervare1.NumeUtilizator} pentru loc {rezervare1.NumarLoc} (Birou)");
+        
         loc1.EsteRezervat = true;
         Console.WriteLine($"Loc {loc1.Numar}: {(loc1.EsteRezervat ? "Rezervat" : "Liber")}");
     }
---
+++ Proiect/Proiect/Rezervare.cs
@@ -0,0 +1,15 @@
+﻿namespace Proiect;
+
+public class Rezervare
+{
+    public string NumeUtilizator { get; set; }
+    public int NumarLoc { get; set; }
+    public bool EsteParcare { get; set; }
+
+    public Rezervare(string numeUtilizator, int numarLoc, bool esteParcare)
+    {
+        NumeUtilizator = numeUtilizator;
+        NumarLoc = numarLoc;
+        EsteParcare = esteParcare;
+    }
+}
